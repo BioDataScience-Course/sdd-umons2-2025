ba02114ba6ec91970b636da7d8c98e0fa646513c
Philippe Grosjean <phgrosjean@sciviews.org>
2025-11-15 11:40:38 +0100

    Link for individual assignment module 4 added

--- setup.R
+++ setup.R
@@ -74,7 +74,7 @@ learnitdown$assign_url <- list(
   B02Ia_achatina       = "https://classroom.github.com/a/179dfmUZ",
   B02Ga_models         = "https://classroom.github.com/a/kaCKEi-H",
   B03Ia_who            = "https://classroom.github.com/a/hH-yrrIc",
-  B04Ia_lungcap        = "https://classroom.github.com/a/...",
+  B04Ia_lungcap        = "https://classroom.github.com/a/8sp4WmRa",
   B05Ia_abies_balsamea = "https://classroom.github.com/a/...",
   B05Ca_models         = "https://classroom.github.com/a/...",
   # Q2
